b865e5c7a9bae1e048ed052928e085153b57dc67
Achim Finke <achim.finke@gmail.com>
2015-12-04 11:13:14 +0100

    Updated to vysper plugin, added logging.

--- src/test/java/org/apache/karaf/test/XMPPIntegrationTest.java
+++ src/test/java/org/apache/karaf/test/XMPPIntegrationTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.karaf.options.LogLevelOption;
 import org.ops4j.pax.exam.options.MavenArtifactUrlReference;
 import org.ops4j.pax.exam.options.MavenUrlReference;
 
@@ -20,15 +21,15 @@ import static org.ops4j.pax.exam.CoreOptions.options;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.*;
 
 @RunWith(PaxExam.class)
-public class XMPPTest extends CamelTestSupport {
+public class XMPPIntegrationTest extends CamelTestSupport {
 
-    private static final String CAMEL_VERSION = "2.14.3";
+    private static final String CAMEL_VERSION = "2.15.4";
 
     private static final MavenArtifactUrlReference KARAF_URL = maven()
             .groupId("org.apache.karaf")
             .artifactId("apache-karaf")
             .type("zip")
-            .version("4.0.1");
+            .version("4.0.3");
     private static final MavenUrlReference CAMEL_FEATURE = maven()
             .groupId("org.apache.camel.karaf")
             .artifactId("apache-camel")
@@ -46,9 +47,11 @@ public class XMPPTest extends CamelTestSupport {
             @Override
             public void configure() throws Exception {
                 from("direct:test")
+                        .to("log:org.apache.karaf.test?level=INFO")
                         .to("xmpp://localhost:5222/camel_consumer@camel.xmpp?user=camel_producer&password=secret&serviceName=camel.xmpp");
 
                 from("xmpp://localhost:5222/?user=camel_consumer&password=secret&serviceName=camel.xmpp")
+                        .to("log:org.apache.karaf.test?level=INFO")
                         .to(mockEndpoint);
 
             }
@@ -60,10 +63,11 @@ public class XMPPTest extends CamelTestSupport {
         return options(
                 karafDistributionConfiguration()
                         .frameworkUrl(KARAF_URL)
-                        .karafVersion("4.0.1")
+                        .karafVersion("4.0.3")
                         .unpackDirectory(new File("target/exam"))
                         .useDeployFolder(false),
                 keepRuntimeFolder(),
+                logLevel(LogLevelOption.LogLevel.INFO),
                 replaceConfigurationFile("etc/keystores/xmppServer.jks", new File("src/test/resources/etc/keystores/xmppServer.jks")),
                 editConfigurationFilePut("etc/system.properties", "javax.net.ssl.trustStore", "${karaf.base}/etc/keystores/xmppServer.jks"),
                 features(CAMEL_FEATURE, "camel-test", "camel-xmpp")
